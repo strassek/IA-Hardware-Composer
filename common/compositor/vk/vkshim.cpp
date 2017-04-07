61cf92bbdc8d3ffda64a3a6a2a245f9de25d882e
Kevin Strasser <kevin.strasser@intel.com>
2017-04-07 07:24:42 -0700

    vk: Wrap global state in VKContext
    Previously all vk objects that needed to be shared between classes were simply
    being tracked at global scope. That made it hard to understand who was in
    charge of creating and destroying them. Wrap all of these objects in VKContext,
    and have each VKRenderer own their own instance. This allows us to have
    VKRenderer::MakeCurrent actually do something meaningful, which is setting a
    global pointer to the current VKContext. When NativeVKResource and VKSurface
    are initialized they then bind to the current global context.
    
    Jira: IAHWC-40
    Test: Check that the test apps are still working
    
    Signed-off-by: Kevin Strasser <kevin.strasser@intel.com>

--- common/compositor/vk/vkshim.cpp
+++ common/compositor/vk/vkshim.cpp
@@ -18,22 +18,6 @@
 
 namespace hwcomposer {
 
-VkDevice dev_;
-VkInstance inst_;
-VkRenderPass render_pass_;
-VkPipelineCache pipeline_cache_;
-VkBuffer uniform_buffer_;
-VkSampler sampler_;
-std::vector<VkImage> src_images_;
-std::vector<VkImageView> src_image_views_;
-std::vector<VkDescriptorImageInfo> src_image_infos_;
-RingBuffer ring_buffer_;
-std::vector<RingBuffer::Allocation> ub_allocs_;
-size_t ub_offset_align_;
-std::vector<VkImageMemoryBarrier> src_barrier_before_clear_;
-VkImageMemoryBarrier dst_barrier_before_clear_;
-VkFramebuffer framebuffer_;
-
 VkFormat DrmToVkFormat(int drm_format) {
   switch (drm_format) {
     case DRM_FORMAT_BGR888:
